27940eccad8536e13ead78aad2f03a82337d3c77
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-12-05 17:42:15 +0200

    Initial commit

---
+++ scrapper.js
@@ -0,0 +1,20 @@
+var request = require("request");
+var fs = require("fs");
+
+request({
+    uri: "http://example.com"
+}, function(error, response, body) {
+    //delete file if exists
+    fs.unlink('webpage.txt', function(notExist) {
+        if (notExist) {
+            console.log("Deleting file if exists...");
+        }
+    });
+
+    //create and write file asynchronously
+    fs.appendFile('webpage.txt', body, function(err) {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+
+});
